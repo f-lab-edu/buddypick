a0c53e23ea47d16ac62061d7d9f9834897a99098
hyerinnn <eun8229@gmail.com>
2024-09-08 01:58:33 +0900

    project push

---
+++ src/main/java/com/buddypick/BuddypickApplication.java
@@ -0,0 +1,13 @@
+package com.buddypick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddypickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddypickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buddypick/BuddypickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buddypick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddypickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
