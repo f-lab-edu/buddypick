cac91898deed13cae9f255ba932ce48003986d17
hyerinnn <eun8229@gmail.com>
2024-09-08 03:59:11 +0900

    Test API 생성 및 확인

---
+++ src/main/java/com/buddypick/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.buddypick;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String basicController(){
+        return "hello";
+    }
+
+}
